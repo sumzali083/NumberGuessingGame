11559d2e81cb0ec591a4ef80b1813b5b18c7d027
sumzali <alisummer066@icloud.com>
2025-08-25 14:42:40 +0100

    added 3 levels easy medium hard

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import random
 
 def main():
-    number = random.randint(0, 10)
-    guess = int(input("Guess a number between 0 and 10: "))
+    # create 3 levels easy,med,hard
+    level = input("Enter your level: ")
+    if level == "easy":
+        number = random.randint(0, 10)
+        upper_limit = 10
+    elif level == "medium":
+        number = random.randint(0, 50)
+        upper_limit = 50
+    elif level == "hard":
+        number = random.randint(0, 100)
+        upper_limit = 100
+
+    guess = int(input(f"Guess a number between 0 and {upper_limit}: "))
 
     while guess != number:
         if guess < number:
@@ -13,8 +24,6 @@ def main():
             guess = int(input("Guess a number between 0 and 10: "))
 
     else: print("You guessed right!")
-
-
 
 if __name__ == '__main__':
     main()
